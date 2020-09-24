036cafe882d0a78eb96c9c997a9db17df2a2e60f
Filip Plass <filip.plass@pslib.cz>
2020-09-24 14:26:29 +0200

    session

---
+++ Services/SessionStorage.cs
@@ -0,0 +1,29 @@
+﻿using AccountsCreate.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AccountsCreate.Helpers;
+
+namespace AccountsCreate.Services
+{
+    public class SessionStorage
+    {
+        readonly ISession _session;
+        const string KEY = "key";
+        public User user { get; set; }
+        public SessionStorage(IHttpContextAccessor hca)
+        {
+            _session = hca.HttpContext.Session;
+        }
+        public void SaveUser(User userr)
+        {
+            _session.Set<User>(KEY, userr);
+        }
+        public User GetUser()
+        {
+            return _session.Get<User>(KEY);
+        }
+    }
+}
