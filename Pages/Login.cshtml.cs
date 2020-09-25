1a7aa5a09a2effb575f8758fa45b0612a5633a03
Filip Plass <filip.plass@pslib.cz>
2020-09-25 13:04:58 +0200

    Session Storage

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AccountsCreate.Helpers;
 using AccountsCreate.Models;
+using AccountsCreate.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,19 +13,29 @@ namespace AccountsCreate.Pages
     public class LoginModel : PageModel
     {
         private AppDbContext _db { get; set; }
+
+        private readonly SessionStorage _ss;
         [BindProperty]
         public User _us { get; set; }
+        public LoginModel(SessionStorage ss, AppDbContext db)
+        {
+            _ss = ss;
+            _db = db;
+        }
         public void OnGet()
         {
             //_us = _db.Users.Where(x => x.Name == );
         }
-       /* public IActionResult OnPost()
-        {
-            if (_db.Users.Contains(u) == true && _db.Users.Contains(v) == true)
+       public IActionResult OnPost()
+        {//TODO
+            if ()
             {
-                return RedirectToPage("ProfilePage");
+
             }
-            else return RedirectToPage("Login");
-        }*/
+            else
+            {
+                return RedirectToPage("Login");
+            }
+        }
     }
 }
